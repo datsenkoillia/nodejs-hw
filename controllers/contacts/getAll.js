b235fc29dc7f930eecd4369efa0d29ca2fea7782
Illia Datsenko <datsenko.i@gmail.com>
2023-07-30 14:31:17 +0300

    Update getAll.js
    favorite filter added

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -3,13 +3,22 @@ import Contact from "../../models/contact.js";
 const getAll = async (req, res, next) => {
   try {
     const { _id: owner } = req.user;
-    const { page = 1, limit = 20 } = req.query;
+    const { page = 1, limit = 20, favorite } = req.query;
     const skip = (page - 1) * limit;
-    const result = await Contact.find({ owner }, "-__v", {
-      skip,
-      limit,
-    }).populate("owner", "-_id email subscription");
-    res.json(result);
+
+    if (favorite) {
+      const result = await Contact.find({ owner, favorite }, "-__v", {
+        skip,
+        limit,
+      }).populate("owner", "-_id email subscription");
+      res.json(result);
+    } else {
+      const result = await Contact.find({ owner }, "-__v", {
+        skip,
+        limit,
+      }).populate("owner", "-_id email subscription");
+      res.json(result);
+    }
   } catch (error) {
     next(error);
   }
